ccfbf7e553697f66a023d2d5485faf75b5bcc408
Shay Barak <shiber@gmail.com>
2013-03-11 20:40:46 +0200

    Implemented read/write

--- hw1/memPage_t.cpp
+++ hw1/memPage_t.cpp
@@ -1,9 +1,19 @@
 #include "memPage_t.h"
 
 void memPage_t::read(void* out, size_t len, unsigned int pos = pos) const {
-	
+	// TODO handle OOB reads
+	for (size_t i = 0; i < len; i++) {
+		*((char*)out)++ = store[pos++];
+	}
+	// Update cursor
+	this->pos = pos;
 }
 
 void memPage_t::write(void* const in, size_t len, unsigned int pos = pos) {
-	
+	// TODO handle OOB writes
+	for (size_t i = 0; i < len; i++) {
+		store[pos++] = *((char*)in)++;
+	}
+	// Update cursor
+	this->pos = pos;
 }
