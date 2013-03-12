c1122617e2e54c64e0b7113676cb8f123bd2e98c
Shay Barak <shiber@gmail.com>
2013-03-12 17:32:43 +0200

    memPool_t read

--- hw1/memPool_t.cpp
+++ hw1/memPool_t.cpp
@@ -1,4 +1,7 @@
 #include "memPool_t.h"
+#include <algorithm>
+
+using namespace std;
 
 memPool_t::memPool_t(size_t page_size, size_t initial_page_capacity) : page_size(page_size) {
 	pages.reserve(initial_page_capacity);
@@ -12,4 +15,35 @@ memPool_t::~memPool_t() {
 	for (vector<memPage_t*>::iterator iter = pages.begin(); iter < pages.end(); iter++) {
 		delete *iter;
 	}
+}
+
+void memPool_t::read(void* out, size_t len, unsigned int pos = pos) const {
+	// TODO handle OOB reads
+	// if ((pos > size) || (pos + len > capacity)) {
+	//     kaki
+	// }
+	// Find page boundaries
+	size_t begin_page = pos / page_size;
+	size_t end_page = (pos + len - 1) / page_size;
+	for (size_t page = begin_page; page <= end_page; page++) {
+		// Read from page
+		size_t to_read = min(len, page_size - (pos % page_size));
+		pages[page]->read(out, to_read, pos % page_size);
+		pos += to_read;
+		len -= to_read;
+		out = ((char*)out) + to_read;
+	}
+}
+
+void memPool_t::write(void* const in, size_t len, unsigned int pos = pos) {
+	// TODO handle OOB writes
+	// if ((pos > size) || (pos + len > capacity)) {
+	//     kaki
+	// }
+	for (size_t i = 0; i < len; i++) {
+		store[pos++] = *((char*)in)++;
+	}
+	// Update cursor and size
+	this->pos = pos;
+	size = max(size, pos);
 }
