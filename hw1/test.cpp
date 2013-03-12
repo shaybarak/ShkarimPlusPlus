99c670e1f4304c41ba660eeb4ccd48a36813b4c0
Shay Barak <shiber@gmail.com>
2013-03-12 20:54:07 +0200

    Skeleton for unit tests

---
+++ hw1/test.cpp
@@ -0,0 +1,20 @@
+#include "memPage_t.h"
+#include "memPool_t.h"
+
+bool memPage_t_test() {
+	memPage_t myPage(1024);
+}
+
+bool memPool_t_test() {
+	memPool_t myPool();
+}
+
+int main() {
+	if (!memPage_t_test()) {
+		return 1;
+	}
+	if (!memPool_t_test()) {
+		return 1;
+	}
+	return 0;
+}
