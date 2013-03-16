96ddd612a632a5fc0a51bbceb2c35bc2b9b479ef
Shay Barak <shiber@gmail.com>
2013-03-16 16:40:47 +0200

    Test for memPage_t

--- hw1/test.cpp
+++ hw1/test.cpp
@@ -3,6 +3,9 @@
 
 class MyTestType {
 public:
+	MyTestType(int int1, int int2, double double1, double double2)
+	  : int1(int1), int2(int2), double1(double1), double2(double2) {}
+
 	bool operator== (MyTestType &left, MyTestType &right) {
     return (
     	left.int1 == right.int1 &&
@@ -20,11 +23,72 @@ public:
 };
 
 bool memPage_t_test() {
+	// Validate init
 	memPage_t myPage(1024);
+	if (myPage.getCapacity() != 1024) {
+		printf("Incorrect capacity %d!\n", myPage.getCapacity());
+		return false;
+	}
+	if (myPage.getSize() != 0) {
+		printf("Incorrect size %d!\n", myPage.getSize());
+		return false;
+	}
+	if (myPage.getPos() != 0) {
+		printf("Incorrect position %d!\n", myPage.getPos());
+		return false;
+	}	
+
+	MyTestType instance1(1, 2, 1.2, 3.4);
+	MyTestType instance2(5, 6, 5.6, 7.8);
+
+	// Validate writes
+	if (!myPage.write(&instance1, sizeof(instance1))) {
+		printf("Write #1 failed!\n");
+		return false;
+	}
+	// Write out of bounds
+	if (myPage.write(&instance1, sizeof(instance1), -3)) {
+		printf("Write #2 unexpectedly succeeded!\n");
+		return false;
+	}
+	// Write out of bounds
+	if (myPage.write(&instance1, sizeof(instance1), sizeof(instance1) + 5)) {
+		printf("Write #3 unexpectedly succeeded!\n");
+		return false;
+	}
+	// Overwrite
+	if (!myPage.write(&instance2, sizeof(instance2), 0)) {
+		printf("Write #4 failed!\n");
+		return false;
+	}
+
+	// Validate reads
+	if (!myPage.read(&instance1, sizeof(instance1), 0)) {
+		printf("Read #1 failed!\n");
+		return false;
+	}
+	// Wrote instance2 and read into instance1 so expect equality
+	if (instance1 != instance2) {
+		printf("Expected equality!\n");
+		return false;
+	}
+	// Read out of bounds
+	if (myPage.read(&instance1, sizeof(instance1), -3)) {
+		printf("Read #2 unexpectedly succeeded!\n");
+		return false;
+	}
+	// Write out of bounds
+	if (myPage.read(&instance1, sizeof(instance1), sizeof(instance1) + 5)) {
+		printf("Read #3 unexpectedly succeeded!\n");
+		return false;
+	}
+
+	return true;
 }
 
 bool memPool_t_test() {
 	memPool_t myPool(1024);
+	return true;
 }
 
 int main() {
