800b7b7c6f2a51fa03ae659899cc1831f449eb9f
Shay Barak <shiber@gmail.com>
2013-03-16 16:14:31 +0200

    Defined a test class for use in pool reads/writes

--- hw1/test.cpp
+++ hw1/test.cpp
@@ -1,6 +1,24 @@
 #include "memPage_t.h"
 #include "memPool_t.h"
 
+class MyTestType {
+public:
+	bool operator== (MyTestType &left, MyTestType &right) {
+    return (
+    	left.int1 == right.int1 &&
+    	left.int2 == right.int2 &&
+    	left.double1 == right.double1 &&
+    	left.double2 == right.double2);
+	}
+ 
+	bool operator!= (MyTestType &left, MyTestType &right) {
+    	return !(left == right);
+	}
+
+	int int1, int2;
+	double double1, double2;
+};
+
 bool memPage_t_test() {
 	memPage_t myPage(1024);
 }
