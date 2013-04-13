2bbbcd2040a7bb0d10046da8c9ee692d25a0d169
Noam <noaminbar510@gmail.com>
2013-04-13 16:51:25 +0300

    working on containertest

--- hw2/ContainerTest.cpp
+++ hw2/ContainerTest.cpp
@@ -1,9 +1,30 @@
 #pragma once
+
+#include <iostream>
+#include <string>
 #include "Container_t.h"
 #include "Array_t.h"
-#include "Dlist_t.h"
+//#include "Dlist_t.h"
 
-int main(int argc, char *argv[]) {
-	Container_t<int>* intCont = new Array_t();
-	return 0;
+using namespace std;
+
+bool Array_t_test() {
+	Array_t<int>* intContArray = new Array_t<int>();
+	for (int i = 0; i < 10; i++) {
+		intContArray->insert(*new int(5 * i));
+	}
+	for (int i = 0; i < 10; i++) {
+		if (intContArray[i] != (5*i)) {
+			cerr << "Incorrect value on intCountArray[" << i 
+				<< "] expected " << 5*i << "got " << *(int*)(intContArray[i]) << endl;
+			return false;
+		}
+	}
+}
+
+int main() {
+	if (!memPage_t_test()) {
+		cerr << "Error in memPage_t test!" << endl;
+		return 1;
+	}
 }
