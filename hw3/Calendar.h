15d13c3186937b090bab6a3bd36ecc7ab3f30615
Noam <noaminbar510@gmail.com>
2013-05-04 13:37:58 +0300

    added print to Calendar

--- hw3/Calendar.h
+++ hw3/Calendar.h
@@ -7,6 +7,7 @@ public:
 	void addAppointment(Day::WeekDay weekDay, const Appointment& appointment);
 	void removeAppointment(Day::WeekDay weekDay, const Appointment::DayTime& startTime);
 	const Appointment& findAppointment(Day::WeekDay weekDay, const Appointment::DayTime& startTime) const;
+	void print();
 
 private:
 	Day days[7];
