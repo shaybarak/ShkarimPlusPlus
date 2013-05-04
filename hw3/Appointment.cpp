670a3105501b414c069259c46cd93f132dbc77dd
Shay Barak <shiber@gmail.com>
2013-05-04 14:24:34 +0300

    Appointment constructor

--- hw3/Appointment.cpp
+++ hw3/Appointment.cpp
@@ -1,1 +1,8 @@
 #include "Appointment.h"
+
+Appointment::Appointment(const string& subject, const DayTime& start, const DayTime& end)
+	: subject(subject), start(start), end(end) {
+		if (end < start) {
+			throw "Appointments cannot end before they start";
+		}
+}
