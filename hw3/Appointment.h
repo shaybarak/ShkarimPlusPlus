830c4e510d149a9fcb521880e971797eb0172ea2
Noam <noaminbar510@gmail.com>
2013-05-04 13:35:40 +0300

    moved sources up the hierarchy

---
+++ hw3/Appointment.h
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Appointment {
+public:
+	typedef pair<int, int> DayTime;
+
+	Appointment(const string& subject, const DayTime& start, const DayTime& end);
+	Appointment(const Appointment& rhs);
+	bool operator==(const Appointment& rhs);
+	void destroy() const { delete this; }
+
+	const string& getSubject() const { return subject; }
+	DayTime getStartTime() const { return startTime; }
+	DayTime getEndTime() const { return endTime; }
+
+protected:
+	// Disallow stack allocation
+	~Appointment() {}
+
+private:
+	const string subject;
+	const DayTime startTime;
+	const DayTime endTime;
+}
---
+++ hw3/Day.h
@@ -0,0 +1,28 @@
+#include "Appointment.h"
+#include <map>
+using namespace std;
+
+class Day {
+public:
+	enum WeekDay {
+		SUNDAY = 0,
+		MONDAY,
+		TUESDAY,
+		WEDNESDAY,
+		THURSDAY,
+		FRIDAY,
+		SATURDAY,
+	};
+
+	Day() {}
+	void addAppointment(const Appointment& appointment);
+	void removeAppointment(const Appointment::DayTime& startTime);
+	const Appointment& findAppointment(const Appointment::DayTime& startTime) const;
+
+private:
+	// Disallow copy constructor
+	Day(Day& rhs) {}
+
+	// Maps appointment start time to appointment
+	map<Appointment::DayTime, Appointment*> appointments;
+}
