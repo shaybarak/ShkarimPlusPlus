da39339c6565379c3e545b6cf2488fb55fd2d2bc
Shay Barak <shiber@gmail.com>
2013-05-04 17:39:00 +0300

    Move appointment

--- hw3/Main.cpp
+++ hw3/Main.cpp
@@ -6,8 +6,9 @@ int main() {
 	Calendar* calendar = new Calendar();
 	bool cont = true;			// trigger to stop loop
 	//unsigned int weekDay;
-	unsigned char weekDay;
+	unsigned char weekDay, newWeekDay;
 	const Appointment* app;
+	const Appointment* newApp;
 	Appointment::DayTime startTime, endTime;
 	string subject;
 	unsigned int c;
@@ -70,6 +71,7 @@ int main() {
 //shay hear!
 
 			case 2:
+				// Find appointment
 				cout << "Enter day for appointment, 1=Sunday...7-saturday" << endl;
 				cin >> weekDay;
 				cout << "Enter start hour for appointment [0-23]" << endl;
@@ -82,6 +84,36 @@ int main() {
 				} else {
 					cout << "Appointment not found!" << endl;
 				}
+				break;
+
+			case 3:
+				// Find existing appointment
+				cout << "Enter day for appointment, 1=Sunday...7-saturday" << endl;
+				cin >> weekDay;
+				cout << "Enter start hour for appointment [0-23]" << endl;
+				cin >> startTime.first;
+				cout << "Enter start minute for appointment [0-59]" << endl;
+				cin >> startTime.second;
+				app = calendar->findAppointment(weekDay, startTime);
+				if (app == NULL) {
+					cout << "Appointment not found!" << endl;
+					break;
+				}
+				// Create new appointment
+				cout << "Enter new day for appointment, 1=Sunday...7-saturday" << endl;
+				cin >> newWeekDay;
+				cout << "Enter new start hour for appointment [0-23]" << endl;
+				cin >> startTime.first;
+				cout << "Enter new start minute for appointment [0-59]" << endl;
+				cin >> startTime.second;
+				cout << "Enter new end hour for appointment [0-23]" << endl;
+				cin >> endTime.first;
+				cout << "Enter new end minute for appointment [0-59]" << endl;
+				cin >> endTime.second;
+				newApp = new Appointment(app->getSubject(), startTime, endTime);
+				// Replace existing appointment with new appointment
+				calendar->removeAppointment(weekDay, app->getStartTime());
+				calendar->addAppointment(newWeekDay, newApp);
 				break;
 
 			default:
