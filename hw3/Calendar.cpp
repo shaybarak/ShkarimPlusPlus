cb913eec23df50e53ccbe64c103ac6f616383d85
Shay Barak <shiber@gmail.com>
2013-05-04 16:58:07 +0300

    Use weekdays

--- hw3/Calendar.cpp
+++ hw3/Calendar.cpp
@@ -21,8 +21,8 @@ void Calendar::print() const{
 	for (set<Appointment::DayTime>::iterator iter = allTimes.begin(); iter != allTimes.end(); iter++) {
 		cout << setw(7 * (maxSubjectLen + 2)) << setfill('-') << "" << endl;
 		cout << setw(maxSubjectLen) << setfill(' ');
-		for (int i = 0; i < 7; i++) {
-			const Appointment* app = days[i].findAppointment(*iter);
+		for (Day::WeekDay weekDay = Day::SUNDAY; weekDay <= Day::SATURDAY; weekDay++) {
+			const Appointment* app = days[weekDay].findAppointment(*iter);
 			subject = (app == NULL ? "" : app->getSubject());
 			cout << left << "|" << subject << "|" << endl;
 		}
