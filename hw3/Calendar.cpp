c38386b07e07c95ef839295ad5f7e1a804db6a57
Noam <noaminbar510@gmail.com>
2013-05-04 16:14:38 +0300

    print fixes

--- hw3/Calendar.cpp
+++ hw3/Calendar.cpp
@@ -3,35 +3,30 @@
 #include <iostream>
 #include "Calendar.h"
 
-void Calendar::print() {
+void Calendar::print() const{
 	set<Appointment::DayTime> allTimes;
-	int maxSubjectLen = 0;
+	unsigned int maxSubjectLen = 0;
 
 	//find all appointments times
 	for (int i = 0; i < 7; i++) {
-		//TODO decide on modifiers here, as appointments is private...
-		const Day::AppMap& appMap = days[i].getAllAppointments();
-		for (Day::AppMap::iterator iter = appMap.begin(); iter != appMap.end(); iter++) {
+		const Day::Appointments& appMap = days[i].getAllAppointments();
+		for (Day::Appointments::iterator iter = appMap.begin(); iter != appMap.end(); iter++) {
 			allTimes.insert(iter->first);
-			maxSubjectLen = max(maxSubjectLen, iter->second.getSubject().len());
+			maxSubjectLen = max(maxSubjectLen, iter->second->getSubject().length());
 		}
 	}
 
 	//print all lines
 	string subject;
 	for (set<Appointment::DayTime>::iterator iter = allTimes.begin(); iter != allTimes.end(); iter++) {
-		cout << printLine(cout, maxSubjectLen);
+		cout << setw(7 * (maxSubjectLen + 2)) << setfill('-') << "" << endl;
 		cout << setw(maxSubjectLen) << setfill(' ');
 		for (int i = 0; i < 7; i++) {
 			const Appointment* app = days[i].findAppointment(*iter);
 			subject = (app == NULL ? "" : app->getSubject());
-			cout << left << "| " << subject << " |" << endl;
+			cout << left << "|" << subject << "|" << endl;
 		}
 	}
-	cout << printLine(cout, maxSubjectLen) << endl;
-}
-
-ostream& Calendar::printLine(ostream& os, int subjectLen) {
-	return os << setw(7 * (subjectLen + 1)) << setfill('-') << "" << endl;
+	cout << setw(7 * (maxSubjectLen + 2)) << setfill('-') << "" << endl;
 }
 
