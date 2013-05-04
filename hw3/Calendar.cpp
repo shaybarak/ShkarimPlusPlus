1bd0d1521dc6b6c221b21bf22a92e710b5397d56
Noam <noaminbar510@gmail.com>
2013-05-04 14:44:46 +0300

    working on calendar.cpp

--- hw3/Calendar.cpp
+++ hw3/Calendar.cpp
@@ -3,8 +3,19 @@
 
 void Calendar::print() {
 	set<Appointment::DayTime> allTimes;
+	int maxSubjectLen = 0;
 	for (int i = 0; i < 7; i++) {
-		
+		Day::AppMap::iterator iter = days[i].getAppointmentIter();
+		//TODO decide on modifiers here, as appointments is private...
+		for (iter = days[i].appointments.begin(); iter != appointments.end(); iter++) {
+			
+		}
 	}
-	while(
+
+	//now print all lines
+	for (set<Appointment::DayTime>::iterator iter = allTimes.begin(); iter != allTimes.end(); iter++) {
+		for (int i = 0; i < 7; i++) {
+			app = 
+		}
+	}
 }
