9082dd10239f5ebdb6ed03ff84b2ccc9e41ed0f8
Shay Barak <shiber@gmail.com>
2013-05-04 13:25:16 +0300

    Headers for all important data types

---
+++ hw3/hw3/Calendar.h
@@ -0,0 +1,13 @@
+#include "Day.h"
+using namespace std;
+
+class Calendar {
+public:
+	Calendar() {}
+	void addAppointment(Day::WeekDay weekDay, const Appointment& appointment);
+	void removeAppointment(Day::WeekDay weekDay, const Appointment::DayTime& startTime);
+	const Appointment& findAppointment(Day::WeekDay weekDay, const Appointment::DayTime& startTime) const;
+
+private:
+	Day days[7];
+}
