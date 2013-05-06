182464c93e59ad1773ac5ebbc6e6dfddd90ce15c
Shay Barak <shiber@gmail.com>
2013-05-06 21:15:49 +0300

    Documented taking ownership of a pointer

--- hw3/Calendar.h
+++ hw3/Calendar.h
@@ -7,7 +7,7 @@ using namespace std;
 class Calendar {
 public:
 	// Parameter weekDay is 1..7 for Sunday..Saturday
-	virtual void addAppointment(int weekDay, const Appointment* appointment);
+	virtual void addAppointment(int weekDay, const Appointment* appointment);  // Takes ownership of appointment
 	virtual void removeAppointment(int weekDay, const Appointment::DayTime& startTime);
 	virtual const Appointment* findAppointment(int weekDay, const Appointment::DayTime& startTime) const;
 	virtual void print() const;
