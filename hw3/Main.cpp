1e1517f5501cb33f22cd8959441e6a0913fd8e35
Shay Barak <shiber@gmail.com>
2013-05-07 03:14:36 -0400

    Handle exceptions in main loop.

--- hw3/Main.cpp
+++ hw3/Main.cpp
@@ -38,85 +38,89 @@ int main() {
 			 << "Enter your choice: ";
 		cin >> c;
 
-		switch (c) {
-			case 0:
-				cout << "Enter day for appointment, 1-Sunday...7-saturday" << endl;
-				cin >> weekDay;
-				cout << "Enter start hour for appointment [0-23]" << endl;
-				cin >> startTime.first;
-				cout << "Enter start minute for appointment [0-59]" << endl;
-				cin >> startTime.second;
-				cout << "Enter end hour for appointment [0-23]" << endl;
-				cin >> endTime.first;
-				cout << "Enter end minute for appointment [0-59]" << endl;
-				cin >> endTime.second;
-				cout << "Enter subject for appointment" << endl;
-				cin.ignore();
-				getline(cin, subject);
-				app = new Appointment(subject, startTime, endTime);
-				try {
-					calendar->addAppointment(weekDay, app);
-				} catch(string strEx) {
-					cout << strEx;
-				}
-
-				break;
-			case 1:
-				cout << "Enter day for appointment, 1=Sunday...7-saturday" << endl;
-				cin >> weekDay;
-				cout << "Enter start hour for appointment [0-23]" << endl;
-				cin >> startTime.first;
-				cout << "Enter start minute for appointment [0-59]" << endl;
-				try {
-					calendar->removeAppointment(weekDay, startTime); //TODO rtry and catch
-				} catch (string strEx) {
-					cout << strEx;
-				} 
-				
-
-			case 2:
-				// Find appointment
-				cout << "Enter day for appointment, 1=Sunday...7-saturday" << endl;
-				cin >> weekDay;
-				cout << "Enter start hour for appointment [0-23]" << endl;
-				cin >> startTime.first;
-				cout << "Enter start minute for appointment [0-59]" << endl;
-				cin >> startTime.second;
-				app = calendar->findAppointment(weekDay, startTime);
-				if (app != NULL) {
-					cout << app << endl;
-				} else {
-					cout << "Appointment not found!" << endl;
-				}
-				break;
-
-			case 3:
-				cout << "Enter current day for appointment, 1=Sunday...7-saturday" << endl;
-				cin >> weekDay;
-				cout << "Enter current start hour for appointment [0-23]" << endl;
-				cin >> startTime.first;
-				cout << "Enter current start minute for appointment [0-59]" << endl;
-				cin >> startTime.second;
-				cout << "Enter new day for appointment, 1=Sunday...7-saturday" << endl;
-				cin >> newWeekDay;
-				cout << "Enter new start hour for appointment [0-23]" << endl;
-				cin >> newStartTime.first;
-				cout << "Enter new start minute for appointment [0-59]" << endl;
-				cin >> newStartTime.second;
-				cout << "Enter new end hour for appointment [0-23]" << endl;
-				cin >> newEndTime.first;
-				cout << "Enter new end minute for appointment [0-59]" << endl;
-				cin >> newEndTime.second;
-				calendar->duplicateAppointment(weekDay, startTime, newWeekDay, newStartTime, newEndTime);
-				break;
-
-			case 4:
-				calendar->print();
-				break;
-
-			default:
-				cont = false;
-				break;
+		try {
+            switch (c) {
+    			case 0:
+    				cout << "Enter day for appointment, 1-Sunday...7-saturday" << endl;
+    				cin >> weekDay;
+    				cout << "Enter start hour for appointment [0-23]" << endl;
+    				cin >> startTime.first;
+    				cout << "Enter start minute for appointment [0-59]" << endl;
+    				cin >> startTime.second;
+    				cout << "Enter end hour for appointment [0-23]" << endl;
+    				cin >> endTime.first;
+    				cout << "Enter end minute for appointment [0-59]" << endl;
+    				cin >> endTime.second;
+    				cout << "Enter subject for appointment" << endl;
+    				cin.ignore();
+    				getline(cin, subject);
+    				app = new Appointment(subject, startTime, endTime);
+    				try {
+    					calendar->addAppointment(weekDay, app);
+    				} catch(string strEx) {
+    					cout << strEx;
+    				}
+    
+    				break;
+    			case 1:
+    				cout << "Enter day for appointment, 1=Sunday...7-saturday" << endl;
+    				cin >> weekDay;
+    				cout << "Enter start hour for appointment [0-23]" << endl;
+    				cin >> startTime.first;
+    				cout << "Enter start minute for appointment [0-59]" << endl;
+    				try {
+    					calendar->removeAppointment(weekDay, startTime); //TODO rtry and catch
+    				} catch (string strEx) {
+    					cout << strEx;
+    				} 
+    				
+    
+    			case 2:
+    				// Find appointment
+    				cout << "Enter day for appointment, 1=Sunday...7-saturday" << endl;
+    				cin >> weekDay;
+    				cout << "Enter start hour for appointment [0-23]" << endl;
+    				cin >> startTime.first;
+    				cout << "Enter start minute for appointment [0-59]" << endl;
+    				cin >> startTime.second;
+    				app = calendar->findAppointment(weekDay, startTime);
+    				if (app != NULL) {
+    					cout << app << endl;
+    				} else {
+    					cout << "Appointment not found!" << endl;
+    				}
+    				break;
+    
+    			case 3:
+    				cout << "Enter current day for appointment, 1=Sunday...7-saturday" << endl;
+    				cin >> weekDay;
+    				cout << "Enter current start hour for appointment [0-23]" << endl;
+    				cin >> startTime.first;
+    				cout << "Enter current start minute for appointment [0-59]" << endl;
+    				cin >> startTime.second;
+    				cout << "Enter new day for appointment, 1=Sunday...7-saturday" << endl;
+    				cin >> newWeekDay;
+    				cout << "Enter new start hour for appointment [0-23]" << endl;
+    				cin >> newStartTime.first;
+    				cout << "Enter new start minute for appointment [0-59]" << endl;
+    				cin >> newStartTime.second;
+    				cout << "Enter new end hour for appointment [0-23]" << endl;
+    				cin >> newEndTime.first;
+    				cout << "Enter new end minute for appointment [0-59]" << endl;
+    				cin >> newEndTime.second;
+    				calendar->duplicateAppointment(weekDay, startTime, newWeekDay, newStartTime, newEndTime);
+    				break;
+    
+    			case 4:
+    				calendar->print();
+    				break;
+    
+    			default:
+    				cont = false;
+    				break;
+    		}
+		} catch (char* message) {
+    	    cerr << "Error: " << message << endl;   
 		}
 	}
 
