cb8a2b5929a2f45cfd76e7a4859423624a30ddf8
Noam <noaminbar510@gmail.com>
2013-05-18 19:41:39 +0300

    Borrower_t.h

--- hw4/hw4/Borrower_t.h
+++ hw4/hw4/Borrower_t.h
@@ -1,3 +1,27 @@
-class Borrower_t {
+#include <string>
+#include <list>
+#include "IDs.h"
 
+using namespace std;
+
+class Borrower_t {
+public:
+	Borrower_t(const string& name, const BorrowerId borrowerId)
+	: name(name), borrowerId(borrowerId){}
+
+	/** Basic queries. */
+	const BorrowerId getBorrowerId() const { return borrowerId; }
+	const string& getName() const { return name; }
+	const list<const ISBN>& getBorrowedBooks() const { return borrowedBooks; }
+
+	/** Mutators. */
+	// borrow a book
+	void borrowBook(ISBN isbn);
+	// Registers that a borrower returned a single copy of the book; throws if borrower not registered as lender
+	void returnBook(ISBN isbn);
+
+private:
+	const string name;
+	BorrowerId borrowerId;
+	list<const ISBN> borrowedBooks;
 };
