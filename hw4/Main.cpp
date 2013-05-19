c25feb97f1335437d71b987e7494326ea24c9c61
Shay Barak <shiber@gmail.com>
2013-05-19 21:57:22 +0300

    Tabification & typo

--- hw4/Main.cpp
+++ hw4/Main.cpp
@@ -6,55 +6,55 @@
 using namespace std;
 
 int main() {
-    Library library;
-    bool cont = true;    		// trigger to stop loop
-    unsigned int choice;
-    
-    cout << "Sssshhhh!! Welcome to the library." << endl;
-    cout << "Please remain quiet for the duration of your visit." << endl;
-    
-    while (cont) {
-    	cout << "	0: Add book"    		    << endl
+	Library_t library;
+	bool cont = true;    		// trigger to stop loop
+	unsigned int choice;
+	
+	cout << "Sssshhhh!! Welcome to the library." << endl;
+	cout << "Please remain quiet for the duration of your visit." << endl;
+	
+	while (cont) {
+		cout << "	0: Add book"    		    << endl
 			 << "	1: Remove book"	            << endl
 			 << "	2: Add borrower"            << endl
 			 << "	3: Remove borrower"         << endl
 			 << "	4: Register a borrow"       << endl
-             << "   5: Register a return"       << endl
-             << "   6: Print books report"      << endl
-             << "   7: Print borrowers report"  << endl
+			 << "   5: Register a return"       << endl
+			 << "   6: Print books report"      << endl
+			 << "   7: Print borrowers report"  << endl
 			 << "Enter your choice: ";
 		cin >> choice;
-        
-        try {
-            switch (choice) {
-                case 0:
-                    break;
-                
-                case 1:
-                    break;
-                    
-                case 2:
-                    break;
-                    
-                case 3:
-                    break;
-                    
-                case 4:
-                    break;
-                    
-                case 5:
-                    break;
-                    
-                case 6:
-                    library.reportBooks(cout);
-                    break;
-                    
-                case 7:
-                    library.reportBorrowers(cout);
-                    break;
-            }
-        } catch (char* message) {
-            cerr << "Error: " << message << endl;
-        }
-    }
+		
+		try {
+			switch (choice) {
+				case 0:
+					break;
+				
+				case 1:
+					break;
+					
+				case 2:
+					break;
+					
+				case 3:
+					break;
+					
+				case 4:
+					break;
+					
+				case 5:
+					break;
+					
+				case 6:
+					library.reportBooks(cout);
+					break;
+					
+				case 7:
+					library.reportBorrowers(cout);
+					break;
+			}
+		} catch (char* message) {
+			cerr << "Error: " << message << endl;
+		}
+	}
 }
