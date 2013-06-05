6cb35f383ddf8e34b469dfb15d9aa5f3ff1b53b7
Shay Barak <shiber@gmail.com>
2013-06-05 08:28:49 -0400

    Subject

--- hw5/Subject.cpp
+++ hw5/Subject.cpp
@@ -1,1 +1,15 @@
 #include "Subject.h"
+#include "Observer.h"
+
+using namespace std;
+
+void Subject::notify_observers() {
+    vector::iterator iter;
+    for (iter = observers.begin(); iter != observers.end(); ++iter) {
+        (*iter)->notify(this);
+    }
+}
+
+void Subject::register_observer(Observer* o) {
+    observers.push_back(o);
+}
--- hw5/Subject.h
+++ hw5/Subject.h
@@ -1,4 +1,20 @@
 #ifndef SUBJECT_H
 #define SUBJECT_H
 
+#include <vector>
+
+using namespace std;
+
+class Subject {
+public:
+    virtual ~Subject() {};
+    void register_observer(Observer* o);
+
+protected:
+    void notify_observers();
+
+private:
+    std::vector observers;
+};
+
 #endif
