5ea8082c70f1298751b9540bd009bcc696479edc
Shay Barak <shiber@gmail.com>
2013-06-07 16:27:22 +0300

    minimum visibility

--- hw5/Faculty.h
+++ hw5/Faculty.h
@@ -10,9 +10,10 @@ public:
 	University::MessageType getLastChangeType() const;
 	unsigned int getTuition() const;
 
-	void update(Subject* subject);
-
 protected:
 	// Message filter, returns true to pass to students
 	virtual bool isRelevant(University::MessageType messageType) const = 0;
+
+private:
+	void update(Subject* subject);
 };
