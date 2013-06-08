aea929a6af245f75bf301317728491641bfde5b1
Noam <noaminbar510@gmail.com>
2013-06-08 15:14:14 +0300

    StudentImpl factory received name, type
    and create the StudentImpl

--- hw5/StudentImplFactory.cpp
+++ hw5/StudentImplFactory.cpp
@@ -1,13 +1,14 @@
 #include "StudentImplFactory.h"
-#include "StudentImpl.h"
+#include "ESFStudentImpl.h"
+#include "FOHStudentImpl.h"
 
-Student* StudentImplFactory::create(const string& name, University::FacultyType facultyType) {
+StudentImpl* StudentImplFactory::create(const string& name, University::FacultyType facultyType) {
 	StudentImpl* impl = NULL;
 	switch (facultyType) {
 	case University::ESF:
-		impl = new StudentImpl(name, facultyType);
+		impl = new ESFStudentImpl(name);
 	case University::FOH:
-		impl = new StudentImpl(name, facultyType);
+		impl = new FOHStudentImpl(name);
 	}
 	return impl;
 }
